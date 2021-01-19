90f63027f0a0fa17c644a830e78e1f68a91a76f2
Ben Chadwick <bchadwick@mail.greenriver.edu>
2021-01-19 14:36:43 -0500

    added h2

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Donut</title>
+</head>
+<body>
+    <h2>Donut</h2>
+</body>
+</html>
 <?php
     echo "<h1>Hello, World!</h1>";
+
