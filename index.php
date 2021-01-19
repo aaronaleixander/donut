521dc1063cbfcef02d9ae38f68a629fe87879f8d
Ben Chadwick <bchadwick@mail.greenriver.edu>
2021-01-19 14:46:08 -0500

    added h6

--- index.php
+++ index.php
@@ -12,5 +12,7 @@
     echo "<h1>Hello, World!</h1>";
     echo "<h3>Donut Flavors</h3>";
     echo "<h4>Donut Toppings</h4>";
-    echo "<h5></h5>";
+    echo "<h5>Donut Toppings</h5>";
+    echo "<h6>End of Donut</h6>";
+
 
