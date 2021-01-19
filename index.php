474f58fcca9af4e03fd82a0dcb940ca8d591af09
Ben Chadwick <bchadwick@mail.greenriver.edu>
2021-01-19 15:13:50 -0500

    restructured DOM

--- index.php
+++ index.php
@@ -5,14 +5,13 @@
     <title>Donut</title>
 </head>
 <body>
+    <h1>Hello, World!</h1>;
     <h2>Donut</h2>
+    <h3>Donut Flavors</h3>;
+    <h4>Donut Toppings</h4>;
+    <h5>Donut Toppings</h5>;
+    <h6>End of Donut</h6>;
 </body>
 </html>
-<?php
-    echo "<h1>Hello, World!</h1>";
-    echo "<h3>Donut Flavors</h3>";
-    echo "<h4>Donut Toppings</h4>";
-    echo "<h5>Donut Toppings</h5>";
-    echo "<h6>End of Donut</h6>";
 
 
